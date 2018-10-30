98e31e05c1308cf970c47754d501c3e3ca1d83cc
prutskov <lehaprutskov@gmail.com>
2018-10-31 01:20:41 +0300

    Added tests for dll

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -1,6 +1,38 @@
 #include "pch.h"
+#include "../export.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(ReverseTests, EvenNumberOfSymbols) {
+	char str[] = "string";
+	reverse(str);
+	ASSERT_STREQ(str, "gnirts");
 }
+
+TEST(ReverseTests, OddNumberOfSymbols) {
+	char str[] = "house";
+	reverse(str);
+	ASSERT_STREQ(str, "esuoh");
+}
+
+TEST(ReverseTests, OneSymbols) {
+	char str[] = "h";
+	reverse(str);
+	ASSERT_STREQ(str, "h");
+}
+
+TEST(ReverseTests, TwoSymbols) {
+	char str[] = "QA";
+	reverse(str);
+	ASSERT_STREQ(str, "AQ");
+}
+
+TEST(ReverseTests, NotCorrectNumOfSynmbols) {
+	char str[] = "memory";
+	reverse(str);
+	ASSERT_STRNE(str, "yromemp");
+}
+TEST(ReverseTests, UnicodeSymbols) {
+	char str[] = "мыло";
+	reverse(str);
+	ASSERT_STREQ(str, "олым");
+}
+
